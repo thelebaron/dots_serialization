625c140c222a0d2571dd234d887e807bf645225f
thelebaron <thelebaron@gmail.com>
2021-07-21 13:47:06 -0400

    prelim package

---
+++ Packages/com.thelebaron.buildtools/Editor/FileManagement.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace thelebaron.BuildTools
+{
+    public static class FileManagement
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target">The folder and its contents to delete</param>
+        /// <param name="contentsOnly">Prevents from deleting the root folder specified(only delete contents)</param>
+        public static void DeleteDirectory(string target, bool contentsOnly = false)
+        {
+            string[] files = Directory.GetFiles(target);
+            string[] dir   = Directory.GetDirectories(target);
+ 
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+ 
+            foreach (string path in dir)
+            {
+                DeleteDirectory(path);
+            }
+ 
+            if(!contentsOnly)
+                Directory.Delete(target, true);
+        }
+    }
+}
---
+++ Packages/com.thelebaron.buildtools/Editor/ProjectBuilder.cs
@@ -0,0 +1,116 @@
+﻿using System.Diagnostics;
+using thelebaron.BuildTools;
+using UnityEditor.AddressableAssets.Settings;
+using Debug = UnityEngine.Debug;
+using Directory = UnityEngine.Windows.Directory;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+// ReSharper disable UnusedMember.Global
+
+namespace Unity.Build
+{
+    public static class ProjectBuilder
+    {
+        [MenuItem("ProjectBuilder/Build(Latest git)")]
+        public static void NewPipelineBuild()
+        {
+            
+            Save();
+            
+            Versioning.ClearConsole();
+            
+            SemVer.BumpPatch();
+            
+            AddressableAssetSettings.BuildPlayerContent();
+            
+            LoadResourceFile();
+
+            OpenBuildFolder();
+        }
+
+        [MenuItem("ProjectBuilder/Delete(Latest build)")]
+        public static void DeleteBuild()
+        {
+            var path = System.IO.Directory.GetCurrentDirectory() + "\\" + "Release" + "\\";
+            FileManagement.DeleteDirectory(path, true);
+        }
+        
+        
+        [MenuItem("ProjectBuilder/Open Build Folder")]
+        public static void Open()
+        {
+            OpenBuildFolder();
+        }
+
+        public static void LoadResourceFile()
+        {
+            Debug.Log("Building project using new build pipeline");
+            //PlayerSettings.bundleVersion = Git.BuildVersion;
+            
+            var buildSettings = (BuildConfiguration)AssetDatabase.LoadAssetAtPath("Packages/com.thelebaron.BuildTools/game.buildconfiguration", typeof(BuildConfiguration)); 
+            
+            //Assert.IsNotNull(buildSettings);
+            buildSettings.Build();
+            
+        }
+
+        private static void Save()
+        {
+            AssetDatabase.SaveAssets();
+            EditorSceneManager.SaveOpenScenes();
+        }
+
+        
+        private static void OpenBuildFolder()
+        {
+            var path = System.IO.Directory.GetCurrentDirectory() + "\\" + "Release" + "\\";
+            if (Directory.Exists(path))
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    Arguments = path,
+                    FileName  = "explorer.exe"
+                };
+
+                Process.Start(processStartInfo);
+            }
+        }
+        // Obsolete but for reference
+        /*
+         *
+
+        
+        [MenuItem("Build/(Obsolete)Windows Standalone")]
+        static void Win64()
+        {
+            //string[] scenes = { "Assets/Scenes/prototypeScene3.unity" };
+            //BuildPipeline.BuildPlayer(scenes, "StandaloneWindows64", BuildTarget.StandaloneWindows64, BuildOptions.None);
+            
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes           = new[] {"Assets/Scenes/protoypeScene3.unity"}; //, "Assets/Scene2.unity"
+            buildPlayerOptions.locationPathName = "F:/builds/il2cpp/cyber/cyber.exe";
+            buildPlayerOptions.target           = BuildTarget.StandaloneWindows64;
+            buildPlayerOptions.options          = BuildOptions.ShowBuiltPlayer;
+            UnityEditor.BuildPipeline.BuildPlayer(buildPlayerOptions);
+            
+        }
+
+        [MenuItem("Build/(Obsolete)UWP")]
+        public static void UWP()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes           = new[] {"Assets/Scenes/protoypeScene3.unity"}; //, "Assets/Scene2.unity"
+            buildPlayerOptions.locationPathName = "F:/builds/uwp/cyber/cyber.exe";
+            buildPlayerOptions.target           = BuildTarget.WSAPlayer;
+            buildPlayerOptions.options          = BuildOptions.ShowBuiltPlayer;
+            UnityEditor.BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+
+        }
+         */
+    }
+}
+
+#endif
